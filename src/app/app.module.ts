d8a453297bc3b1f63b4e47d7e7bd2bf982708c03
AbiramiValentina <abitina2002@gmail.com>
2022-12-30 03:02:13 +0530

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { DashComponent } from './dash/dash.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
     AppComponent,
     TextcomponentsComponent,
-    NavComponent
+    NavComponent,
+    DashComponent
   ],
   imports: [
     BrowserModule,
@@ -31,6 +36,9 @@ import { MatListModule } from '@angular/material/list';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
